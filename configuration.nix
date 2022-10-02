ceb9677e4f2b82fb23812d9e8a57a553f8737638
Patrick Aldis <patrick.aldis@virginmedia.com>
2022-10-02 12:52:39 +0100

    add nix-community cachix

--- configuration.nix
+++ configuration.nix
@@ -22,8 +22,8 @@ in
   #DEVICE CONFIGURATION
   nixpkgs.config.allowUnfree = true;                                #NIX
   nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    substituters = ["https://hyprland.cachix.org"  "https://nix-community.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
     experimental-features = [ "nix-command" "flakes" ];
   };
   #nix.nixPath = [ "nixos-config=/home/patrickaldis/.nixrepo/configuration.nix" "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos" "/nix/var/nix/profiles/per-user/root/channels"];
