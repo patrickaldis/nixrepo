46731cf4bc868afdfce2c3e2b636bfd1ca6cfb60
Patrick Aldis <patrick.aldis@virginmedia.com>
2022-10-28 00:22:55 +0100

    add battery script

--- xdgconf/hypr/batteryscript.sh
+++ xdgconf/hypr/batteryscript.sh
@@ -7,11 +7,11 @@ while true; do
 	status="$(echo "$acpi" | grep -P -o '(?<=: )[^,]+')"
 
 	if [ "$status" = "Discharging" ]; then
-		if [ "$need_notify_critical" -eq 1 ] && [ "$percent" -le 5]; then
+		if [ "$need_notify_critical" -eq 1 ] && [ "$percent" -le 5 ]; then
 			notify-send -u critical -i /usr/share/icons/Adwaita/48x48/legacy/battery-caution.png "Battery critical" "$percent% remaining"
 			need_notify_critical=0
 			need_notify_low=0
-		elif [ "$need_notify_low" -eq 1 ] && [ "$percent" -le 15]; then
+		elif [ "$need_notify_low" -eq 1 ] && [ "$percent" -le 15 ]; then
 			notify-send -i /usr/share/icons/Adwaita/48x48/legacy/battery-low.png "Battery low" "$percent% remaining"
 			need_notify_low=0
 		fi
