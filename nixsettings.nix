c34778419ed5e0b13f268f96077ce8fe9c78e343
Patrick Aldis <patrick.aldis@virginmedia.com>
2022-11-01 08:36:29 +0000

    updated cache for nix-gaming

--- nixsettings.nix
+++ nixsettings.nix
@@ -3,9 +3,11 @@
   nix.settings = {
     substituters =
       ["https://hyprland.cachix.org"
+       "https://nix-gaming.cachix.org"
        "https://nix-community.cachix.org"];
     trusted-public-keys =
       ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
     experimental-features =
       ["nix-command"
