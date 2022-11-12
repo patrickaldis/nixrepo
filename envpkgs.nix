6426402fbac57d715be8b13b1537bef42af734b3
Patrick Aldis <patrick.aldis@virginmedia.com>
2022-11-12 13:20:44 +0000

    use use-package

--- envpkgs.nix
+++ envpkgs.nix
@@ -59,67 +59,11 @@
       (emacsWithPackagesFromUsePackage {
         package = pkgs.emacsPgtkNativeComp;
         config = ./xdgconf/emacs/init.el;
+        alwaysEnsure = true;
 
         extraEmacsPackages = (epkgs:
-          (with epkgs; [
-            use-package
-
-            magit
-            git-gutter
-            git-gutter-fringe
-            
-            projectile
-            
-            treemacs
-            all-the-icons
-            treemacs-all-the-icons
-            treemacs-projectile
-            treemacs-magit
-
-            libvterm
-            vterm
-            direnv
-            doom-modeline
-
-            counsel
-            ivy-rich
-            all-the-icons-ivy-rich
-            prescient
-            ivy-prescient
-            avy
-            
-            nix-mode
-            haskell-mode
-            fish-mode
-            
-            evil
-            evil-collection
-            evil-leader
-
-            autothemer
-            dracula-theme
-          ])
-          ++
           (with pkgs;
             [python3]
-          )
-          ++
-          (with epkgs.melpaStablePackages;
-            [
-              lsp-mode
-              lsp-ui
-              company
-              lsp-treemacs
-            ])
-          ++
-          (with epkgs.melpaPackages;
-            [
-              lsp-haskell
-
-              python-mode
-              lsp-pyright
-              flycheck
-            ]
           )
         );
       })
@@ -137,10 +81,6 @@
   {
     flatpak.enable = true;
     lorri.enable = true;
-    # emacs = {
-    #   enable = false;
-    #   package = pkgs.emacs;
-    # };
     udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
   };
 
--- xdgconf/emacs/init.el
+++ xdgconf/emacs/init.el
@@ -1,18 +1,22 @@
 ;; Reqired Packages
+(setq use-package-always-ensure t)
 
-(require 'magit)
-(require 'use-package)
+(use-package magit)
 
-(require 'git-gutter)
-(add-hook 'prog-mode-hook 'git-gutter-mode) 
-(setq git-gutter:update-interval 0.02)
-(require 'git-gutter-fringe)
-(define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
-(define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
-(define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom)
+(use-package git-gutter
+  :config
+  (add-hook 'prog-mode-hook 'git-gutter-mode)
+  (setq git-gutter:update-interval 0.02)
 
-(require 'projectile)
-(projectile-mode +1)
+  (use-package git-gutter-fringe
+    :config
+    (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+    (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+    (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom)))
+
+(use-package projectile
+  :config
+  (projectile-mode +1))
 
 ;; Keymap
 (global-set-key (kbd "C-x p p") 'projectile-switch-project)
@@ -22,134 +26,149 @@
 (global-set-key (kbd "C-x o p") 'treemacs)
 (global-set-key (kbd "C-x o t") 'vterm)
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(require 'autothemer)
-(load-theme 'catppuccin-frappe t)
 
-;; PROJECT VIEWER
-(require 'all-the-icons)
+(use-package autothemer
+  :init (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+  :config (load-theme 'catppuccin-frappe t))
 
-;; (require 'neotree)
-;; (setq neo-theme 'icons)
-;; (setq projectile-switch-project-action 'neotree-projectile-action)
+;; (require 'centaur-tabs)
+;; (centaur-tabs-mode t)
 
-(require 'treemacs)
-(require 'treemacs-all-the-icons)
-(require 'treemacs-magit)
-(require 'treemacs-projectile)
-(treemacs-load-theme "all-the-icons")
-(treemacs-project-follow-mode 1)
+;; ;; PROJECT VIEWER
+(use-package treemacs
+  :config
+  (use-package treemacs-all-the-icons
+    :init (use-package all-the-icons)
+    :config (treemacs-load-theme "all-the-icons"))
+  (use-package treemacs-projectile
+    :config (treemacs-project-follow-mode 1))
+  (use-package treemacs-magit))
 
-(require 'doom-modeline)
-(doom-modeline-mode 1)
+(use-package doom-modeline :config (doom-modeline-mode 1))
 
-(require 'vterm)
+(use-package vterm)
 
-(require 'avy)
-(require 'ivy)
-(require 'ivy-rich)
+(use-package avy)
+(use-package ivy 
+  :init (use-package counsel)
+  :config
+  (ivy-mode 1)
+  (use-package ivy-rich
+    :init 
+    :config
+    (ivy-rich-mode 1)
+    (use-package all-the-icons-ivy-rich
+	:init (use-package all-the-icons)
+	:config (all-the-icons-ivy-rich-mode 1)))
+  (use-package ivy-prescient
+    :init (use-package prescient)
+    :config (ivy-prescient-mode))
+   
+  (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line)
+  :bind (:map ivy-minibuffer-map
+		("C-j" . ivy-next-line)
+		("C-k" . ivy-previous-line)))
 
-(ivy-mode 1)
-(define-key ivy-minibuffer-map (kbd "C-j") 'ivy-next-line)
-(define-key ivy-minibuffer-map (kbd "C-k") 'ivy-previous-line)
-
-(ivy-rich-mode 1)
-(all-the-icons-ivy-rich-mode 1)
-(setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line)
-
-(require 'prescient)
-(require 'ivy-prescient)
-(ivy-prescient-mode)
-
-(require 'direnv)
-(setq direnv-always-show-summary nil)
-(direnv-mode)
+(use-package direnv
+  :config
+  (setq direnv-always-show-summary nil)
+  (direnv-mode)
+  )
 
 ;;COMPANY
-(require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
-(setq company-idle-delay 0)
+(use-package company
+  :config
+  (add-hook 'after-init-hook 'global-company-mode)
+  (setq company-idle-delay 0))
 
-(require 'flycheck)
-(add-hook 'after-init-hook 'global-flycheck-mode)
+(use-package flycheck
+  :config (add-hook 'after-init-hook 'global-flycheck-mode))
 
 ;; LSP
-(require 'lsp-mode)
 (use-package lsp-mode
   :after (evil direnv)
   :config
+  (use-package lsp-ui)
+  (use-package lsp-haskell
+    :init (use-package haskell-mode)
+    :config
+    (add-hook 'haskell-mode-hook #'lsp-deferred)
+    (add-hook 'haskell-literate-mode-hook #'lsp))
+  
   (setq gc-cons-threshold 1000000000)
-  (setq read-process-output-max (* 1024 1024)) ;; 1mb
-  )
+  (setq read-process-output-max (* 1024 1024)))
 
-(require 'lsp-ui)
-(setq gc-cons-threshold 1000000000)
-(setq read-process-output-max (* 1024 1024)) ;; 1mb
+(use-package haskell-mode)
+(use-package nix-mode :config (add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode)))
+(use-package fish-mode)
 
-(require 'haskell-mode)
-(require 'lsp-haskell)
-(add-hook 'haskell-mode-hook #'lsp-deferred)
-(add-hook 'haskell-literate-mode-hook #'lsp)
-
-(require 'nix-mode)
-(add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode))
-
-(require 'fish-mode)
-
-(setq evil-want-keybinding nil)
-(require 'evil-collection)
-(evil-collection-init)
-
-(require 'evil)
-(evil-mode 1)
-(define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
-(define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
-(define-key evil-insert-state-map (kbd "C-u")
-  (lambda ()
-    (interactive)
-    (evil-delete (point-at-bol) (point))))
+(use-package evil
+  :init
+  (setq evil-want-keybinding nil)
+  (use-package evil-collection :config (evil-collection-init))
+  
+  :config
+  (evil-mode 1)
+  (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+  (define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
+  (define-key evil-insert-state-map (kbd "C-u")
+    (lambda ()
+      (interactive)
+      (evil-delete (point-at-bol) (point))))
+  (use-package evil-leader
+    :config
+    (define-key evil-motion-state-map (kbd "SPC") nil)
+    (global-evil-leader-mode)
+    (evil-leader/set-leader "<SPC>")
+    (evil-leader/set-key
+      "f" 'counsel-find-file
+      "p" 'projectile-switch-project
+      "b" 'ivy-switch-buffer
+      "g" 'magit
+      "t" 'vterm
+      "SPC" 'projectile-find-file)))
 
 ;; UI
 ;; FONT
 (setq default-frame-alist '((font . "JetBrainsMono Nerd Font Mono 10")))
 (set-frame-font "JetBrainsMono Nerd Font Mono 10" nil t)
-;; (dolist (char/ligature-re
-;;          `((?-  . ,(rx (or (or "-->" "-<<" "->>" "-|" "-~" "-<" "->") (+ "-"))))
-;;            (?/  . ,(rx (or (or "/==" "/=" "/>" "/**" "/*") (+ "/"))))
-;;            (?*  . ,(rx (or (or "*>" "*/") (+ "*"))))
-;;            (?<  . ,(rx (or (or "<<=" "<<-" "<|||" "<==>" "<!--" "<=>" "<||" "<|>" "<-<"
-;;                                "<==" "<=<" "<-|" "<~>" "<=|" "<~~" "<$>" "<+>" "</>"
-;;                                "<*>" "<->" "<=" "<|" "<:" "<>"  "<$" "<-" "<~" "<+"
-;;                                "</" "<*")
-;;                            (+ "<"))))
-;;            (?:  . ,(rx (or (or ":?>" "::=" ":>" ":<" ":?" ":=") (+ ":"))))
-;;            (?=  . ,(rx (or (or "=>>" "==>" "=/=" "=!=" "=>" "=:=") (+ "="))))
-;;            (?!  . ,(rx (or (or "!==" "!=") (+ "!"))))
-;;            (?>  . ,(rx (or (or ">>-" ">>=" ">=>" ">]" ">:" ">-" ">=") (+ ">"))))
-;;            (?&  . ,(rx (+ "&")))
-;;            (?|  . ,(rx (or (or "|->" "|||>" "||>" "|=>" "||-" "||=" "|-" "|>"
-;;                                "|]" "|}" "|=")
-;;                            (+ "|"))))
-;;            (?.  . ,(rx (or (or ".?" ".=" ".-" "..<") (+ "."))))
-;;            (?+  . ,(rx (or "+>" (+ "+"))))
-;;            (?\[ . ,(rx (or "[<" "[|")))
-;;            (?\{ . ,(rx "{|"))
-;;            (?\? . ,(rx (or (or "?." "?=" "?:") (+ "?"))))
-;;            (?#  . ,(rx (or (or "#_(" "#[" "#{" "#=" "#!" "#:" "#_" "#?" "#(")
-;;                            (+ "#"))))
-;;            (?\; . ,(rx (+ ";")))
-;;            (?_  . ,(rx (or "_|_" "__")))
-;;            (?~  . ,(rx (or "~~>" "~~" "~>" "~-" "~@")))
-;;            (?$  . ,(rx "$>"))
-;;            (?^  . ,(rx "^="))
-;;            (?\] . ,(rx "]#"))))
-;;   (let ((char (car char/ligature-re))
-;;         (ligature-re (cdr char/ligature-re)))
-;;     (set-char-table-range composition-function-table char
-;;                           `([,ligature-re 0 font-shape-gstring]))))
+(dolist (char/ligature-re
+         `((?-  . ,(rx (or (or "-->" "-<<" "->>" "-|" "-~" "-<" "->") (+ "-"))))
+           (?/  . ,(rx (or (or "/==" "/=" "/>" "/**" "/*") (+ "/"))))
+           (?*  . ,(rx (or (or "*>" "*/") (+ "*"))))
+           (?<  . ,(rx (or (or "<<=" "<<-" "<|||" "<==>" "<!--" "<=>" "<||" "<|>" "<-<"
+                               "<==" "<=<" "<-|" "<~>" "<=|" "<~~" "<$>" "<+>" "</>"
+                               "<*>" "<->" "<=" "<|" "<:" "<>"  "<$" "<-" "<~" "<+"
+                               "</" "<*")
+                           (+ "<"))))
+           (?:  . ,(rx (or (or ":?>" "::=" ":>" ":<" ":?" ":=") (+ ":"))))
+           (?=  . ,(rx (or (or "=>>" "==>" "=/=" "=!=" "=>" "=:=") (+ "="))))
+           (?!  . ,(rx (or (or "!==" "!=") (+ "!"))))
+           (?>  . ,(rx (or (or ">>-" ">>=" ">=>" ">]" ">:" ">-" ">=") (+ ">"))))
+           (?&  . ,(rx (+ "&")))
+           (?|  . ,(rx (or (or "|->" "|||>" "||>" "|=>" "||-" "||=" "|-" "|>"
+                               "|]" "|}" "|=")
+                           (+ "|"))))
+           (?.  . ,(rx (or (or ".?" ".=" ".-" "..<") (+ "."))))
+           (?+  . ,(rx (or "+>" (+ "+"))))
+           (?\[ . ,(rx (or "[<" "[|")))
+           (?\{ . ,(rx "{|"))
+           (?\? . ,(rx (or (or "?." "?=" "?:") (+ "?"))))
+           (?#  . ,(rx (or (or "#_(" "#[" "#{" "#=" "#!" "#:" "#_" "#?" "#(")
+                           (+ "#"))))
+           (?\; . ,(rx (+ ";")))
+           (?_  . ,(rx (or "_|_" "__")))
+           (?~  . ,(rx (or "~~>" "~~" "~>" "~-" "~@")))
+           (?$  . ,(rx "$>"))
+           (?^  . ,(rx "^="))
+           (?\] . ,(rx "]#"))))
+  (let ((char (car char/ligature-re))
+        (ligature-re (cdr char/ligature-re)))
+    (set-char-table-range composition-function-table char
+                          `([,ligature-re 0 font-shape-gstring]))))
 
-;; ;; SCROLLING
-;(pixel-scroll-precision-mode 1)
+;; SCROLLING
+(pixel-scroll-precision-mode 1)
 (setq scroll-conservatively 101)
 
 ;; HIDE UI
@@ -166,20 +185,6 @@
 (global-hl-line-mode 1) ;; HIGHLIGHT LINES
 (set-default 'truncate-lines 0) ;; DO NOT WRAP
 (defalias 'yes-or-no-p 'y-or-n-p)
-
-(require 'evil-leader)
-(define-key evil-motion-state-map (kbd "SPC") nil)
-
-(global-evil-leader-mode)
-(evil-leader/set-leader "<SPC>")
-(evil-leader/set-key
-  "f" 'counsel-find-file
-  "p" 'projectile-switch-project
-  "b" 'ivy-switch-buffer
-  "g" 'magit
-  "t" 'vterm
-  "SPC" 'projectile-find-file
-  )
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
