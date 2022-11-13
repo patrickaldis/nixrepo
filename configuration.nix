714abed91bb93e2f75e8798d0679c60c3f6b8aa2
Patrick Aldis <patrick.aldis@virginmedia.com>
2022-11-13 23:22:02 +0000

    clean up configuration file

--- configuration.nix
+++ configuration.nix
@@ -2,12 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, inputs, ... }: # let                                                                                        #
-#  home-manager = builtins.fetchTarball {                                                                                   #
-#     url = "https://github.com/nix-community/home-manager/archive/a7f0cc2d7b271b4a5df9b9e351d556c172f7e903/master.tar.gz"; #
-#     sha256="0ydkwprdkiq3xjkqf8j7spwaaaxhdm2ka2ylkzycfz49id4fb2q2";                                                        #
-#   };                                                                                                                      #
-# in                                                                                                                        #
+{ config, pkgs, inputs, ... }:
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -23,35 +18,6 @@
   fonts.fonts = with pkgs; [
     (nerdfonts.override { fonts = ["JetBrainsMono"]; })
   ];
-
-  ##############################################################################
-  # home-manager.users.patrickaldis = {                                        #
-  #   programs.firefox = {                                                     #
-  #     enable = true;                                                         #
-  #     package = pkgs.firefox.override {                                      #
-  #       cfg = { enableTridactylNative = true; };                             #
-  #     };                                                                     #
-  #   };                                                                       #
-  #                                                                            #
-  #   home.homeDirectory = "/home/patrickaldis";                               #
-  #   home.packages = with pkgs; [ git lutris];                                #
-  #   home.stateVersion = "22.05";                                             #
-  #   home.file.".internet/comodo.pem".source = ./xdgconf/internet/comodo.pem; #
-  #   home.file.".emacs.d/init.el".source = ./xdgconf/emacs/init.el;           #
-  #   xdg.configFile = {                                                       #
-  #     "hypr/hyprland.conf".source = ./xdgconf/hyprland/hyprland.conf;        #
-  #     "waybar".source = ./xdgconf/waybar;                                    #
-  #     "wofi".source = ./xdgconf/wofi;                                        #
-  #     "dunst".source = ./xdgconf/dunst;                                      #
-  #     "fish/config.fish".source = ./xdgconf/fish/config.fish;                #
-  #   };                                                                       #
-  #                                                                            #
-  #   services.dunst = {                                                       #
-  #     enable = true;                                                         #
-  #   };                                                                       #
-  #       };                                                                   #
-  ##############################################################################
-
 
   nixpkgs.overlays = [
     (import (builtins.fetchTarball {
