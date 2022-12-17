84f50ab11ef39725efd5124362d9a06080ea05e5
Patrick Aldis <patrick.aldis@virginmedia.com>
2022-12-17 23:46:27 +0000

    added cabal config

--- shared/home.nix
+++ shared/home.nix
@@ -35,7 +35,7 @@ in
     programs.doom-emacs = {
       enable = true;
       doomPrivateDir = ../xdgconf/doom;
-      doomPackageDir = ../xdgconf/doompkgs;
+      # doomPackageDir = ../xdgconf/doompkgs;
       # emacsPackages = pkgs.emacsPackagesFor (pkgs.emacs.override { withPgtk = true; });
     };
   
@@ -161,18 +161,19 @@ in
     home.homeDirectory = "/home/patrickaldis";
     home.packages = with pkgs; [ git lutris];
     home.stateVersion = "22.05";
+
     # Config Files:
-    home.file.".internet/comodo.pem".source = "${dotfiles}/internet/comodo.pem";
-    # home.file.".emacs.d/init.el".source = "${dotfiles}/emacs/init.el";
-    home.file.".emacs.d/themes/catppuccin-frappe-theme.el".source = "${dotfiles}/emacs/catppuccin-frappe-theme.el";
-    home.file.".themes/CatppuccinFrappe".source = "${dotfiles}/gtk/Catppuccin-Frappe-Pink/Catppuccin-Frappe-Pink";
-    home.file."Pictures/someday_UltraHD.png".source = "${dotfiles}/hypr/hyprpaper/someday_UltraHD.png";
+    home.file = {
+      ".internet/comodo.pem".source = "${dotfiles}/internet/comodo.pem";
+      ".emacs.d/themes/catppuccin-frappe-theme.el".source = "${dotfiles}/emacs/catppuccin-frappe-theme.el";
+      ".themes/CatppuccinFrappe".source = "${dotfiles}/gtk/Catppuccin-Frappe-Pink/Catppuccin-Frappe-Pink";
+      ".cabal/config".source = "${dotfiles}/cabal/config";
+      "Pictures/someday_UltraHD.png".source = "${dotfiles}/hypr/hyprpaper/someday_UltraHD.png";
+    };
 
     xdg.configFile = {
       "hypr/hyprpaper.conf".source = "${dotfiles}/hypr/hyprpaper/hyprpaper.conf";
       "hypr/batteryscript.sh".source = "${dotfiles}/hypr/batteryscript.sh";
-      # "wofi".source = "${dotfiles}/wofi";
-      # "dunst".source = "${dotfiles}/dunst";
       "stig".source = "${dotfiles}/stig";
       "fish/config.fish".source = "${dotfiles}/fish/config.fish";
       "ranger/rifle.conf".source = "${dotfiles}/ranger/rifle.conf";
