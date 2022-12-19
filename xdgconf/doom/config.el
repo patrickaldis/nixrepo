2cd2b9a9e06addcd8c112ff4c2de80b6427f46b9
Patrick Aldis <patrick.aldis@virginmedia.com>
2022-12-19 21:39:32 +0000

    formatted doom files

--- xdgconf/doom/config.el
+++ xdgconf/doom/config.el
@@ -1,134 +1,118 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Patrick Aldis"
       user-mail-address "patrick.aldis@virginmeda.com")
-(setq text-quoting-style "grave")
-(setq byte-compile-warnings nil)
-(setq doom-modeline-major-mode-icon t)
 
-(setq org-preview-latex-default-process 'dvisvgm)
-(setq org-directory "~/Documents/Notes")
-(setq org-agenda-files "~/Documents/Notes/Agenda")
+;; GENERAL OPTIONS ------------------------------------------------------------
+(setq display-line-numbers-type t)              ;; Display line numbers
+(setq doom-modeline-major-mode-icon t)          ;; Show icon in modeline
+(setq text-quoting-style "grave")               ;; Errrrr `' - I think ?
 
-(defadvice org-export-output-file-name (before org-add-export-dir activate)
-  "Modifies org-export to place exported files in a different directory"
-  (when (not pub-dir)
-      (setq pub-dir "exports")
-      (when (not (file-directory-p pub-dir))
-       (make-directory pub-dir))))
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
+;; THEME
+(setq doom-theme 'doom-one)                     ;; Theme :)
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-
-;; FONT STUFF
+;; FONTS
 (setq fontsize 28)
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font Mono" :size fontsize)) ;;
-(setq doom-variable-pitch-font (font-spec :family "JetBrainsMono Nerd Font Mono" :size fontsize)) ;;
+(setq doom-font                                 ;; Monospaced Font
+      (font-spec :family "JetBrainsMono Nerd Font Mono"
+                 :size fontsize))
+(setq doom-variable-pitch-font                  ;; Non-mono (Treemacs)
+      (font-spec :family "JetBrainsMono Nerd Font Mono"
+                 :size fontsize))
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-
-
-(after! lsp-ui
-  (setq lsp-ui-doc-show-with-mouse t)
-  (setq lsp-ui-doc-delay 0.01))
-(after! org
-(setq org-format-latex-options '(:foreground default :background default :scale 0.4 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
-             ("begin" "$1" "$" "$$" "\\(" "\\["))
-
-))
-(map! :leader
-      :desc "Format " "c f" #'lsp-format-buffer)
-(map! :after avy-flycheck
-      :desc "avy goto lens" :n "g s L" #'lsp-avy-lens
-      :desc "avy goto lens" :n "g s l" #'avy-goto-line
-      :desc "avy goto error" :n "g s e" #'avy-flycheck-goto-error)
-
-(after! treemacs (treemacs-project-follow-mode))
-(after! treemacs (setq treemacs-show-cursor t))
-
-;;BIBLIO
-  (after! citar
-    (setq! citar-bibliography '("~/Documents/Citar/Bib/references.bib"))
-    (setq! citar-library-paths '("~/Documents/Citar/Lib"))
-    (setq! citar-notes-paths '("~/Documents/Notes")))
-
-(use-package! all-the-icons-completion :after vertico :config (all-the-icons-completion-mode))
-(use-package! treemacs-all-the-icons :after treemacs :config (setq doom-themes-treemacs-theme "all-the-icons"))
-(use-package! helm-icons :after helm :config (helm-icons-enable))
-(use-package! avy-flycheck :after avy)
-(after! whitespace
-  (setq whitespace-style '(face tabs tab-mark spaces space-mark trailing lines-tail indentation))
-  (global-whitespace-mode t))
+;; BACKUPS
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
     backup-by-copying t    ; Don't delink hardlinks
     version-control t      ; Use version numbers on backups
     delete-old-versions t  ; Automatically delete excess backups
     kept-new-versions 20   ; how many of the newest versions to keep
     kept-old-versions 5    ; and how many of the old
-    )
+)
 
-(add-hook 'after-make-frame-functions
-          (lambda (frame)
-            (doom/reload-font)
-            ))
+;; EDITOR ----------------------------------------------------------------------
+;; TREEMACS
+(after! treemacs
+  (treemacs-project-follow-mode)                ;; Projectile/Treemacs switch
+  (setq treemacs-show-cursor t)                 ;; Show Cursor
+  (use-package! treemacs-all-the-icons          ;; Enable icons
+    :config (setq doom-themes-treemacs-theme "all-the-icons"))
+)
 
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+;; VERTICO
+(after! vertico
+  (use-package! all-the-icons-completion        ;; Enable Icons
+    :config (all-the-icons-completion-mode))
+)
+
+;; CODE ------------------------------------------------------------------------
+;; LSP
+(after! lsp-ui
+  (setq lsp-ui-doc-show-with-mouse t)   ;; Override doom's default hiding hover
+  (setq lsp-ui-doc-delay 0.01)          ;; Show docs instantly
+)
+(map! :leader                           ;; Currently SPC c f doesn't work - FIX
+      :desc "Format Buffer" "c f" #'lsp-format-buffer
+)
+
+;; WHITESPACE
+(after! whitespace
+  (setq whitespace-style
+        '(face
+          tabs
+          tab-mark
+          spaces
+          space-mark
+          trailing
+          lines-tail
+          indentation))
+  (global-whitespace-mode t))
+
+;; AVY
+(after! avy
+  (use-package! avy-flycheck
+    :config
+    (map! :desc "avy goto lens" :n "g s L" #'lsp-avy-lens
+          :desc "avy goto lens" :n "g s l" #'avy-goto-line
+          :desc "avy goto error" :n "g s e" #'avy-flycheck-goto-error)
+  )
+)
+
+;; ORG MODE OPTIONS ------------------------------------------------------------
+(after! org
+  ;; GENERAL
+  ;; Org Directories
+  (setq org-directory "~/Documents/Notes")
+  (setq org-agenda-files "~/Documents/Notes/Agenda")
+  ;; rename export directory to "export"
+  (defadvice org-export-output-file-name (before org-add-export-dir activate)
+    (when (not pub-dir)
+      (setq pub-dir "exports")
+      (when (not (file-directory-p pub-dir))
+        (make-directory pub-dir))))
+
+  ;; LATEX PREVIEW
+  ;; Make scale smaller
+  (setq org-format-latex-options '(:foreground deafult
+                                   :background default
+                                   :scale 0.4
+                                   :html-foreground "Black"
+                                   :html-background "Transparent"
+                                   :html-scale 1.0
+                                   :matchers ("begin" "$1" "$" "$$" "\\(" "\\[")))
+  ;; Render as SVG
+  (setq org-preview-latex-default-process 'dvisvgm)
+)
+
+;;BIBLIO
+(after! citar
+  (setq! citar-bibliography '("~/Documents/Citar/Bib/references.bib"))
+  (setq! citar-library-paths '("~/Documents/Citar/Lib"))
+  (setq! citar-notes-paths '("~/Documents/Notes"))
+)
+
+;;"I'm not really sure if these options do anything"
+;; (use-package! helm-icons :after helm :config (helm-icons-enable))
+;; (add-hook 'after-make-frame-functions
+;;           (lambda (frame)
+;;             (doom/reload-font)
+;;             ))
+;; (setq byte-compile-warnings nil)
--- xdgconf/doom/packages.el
+++ xdgconf/doom/packages.el
@@ -1,17 +1,15 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-;; To install a package with Doom you must declare them here and run 'doom sync'
-;; on the command line, then restart Emacs for the changes to take effect -- or
-;; use 'M-x doom/reload'.
-
-
 (package! all-the-icons-completion)
 (package! helm-icons)
 (package! treemacs-all-the-icons)
 (package! biblio)
 (package! biblio-core)
 (package! avy-flycheck)
+
+;; DOOM DOCS -------------------------------------------------------------------
+
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 
