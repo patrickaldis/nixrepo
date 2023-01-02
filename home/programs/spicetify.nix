ca02eb0e15f0e273bcde37e5a12612291d5d6e74
Patrick Aldis <patrick.aldis@virginmedia.com>
2023-01-02 13:48:24 +0000

    update spicetify + syntax

--- home/programs/spicetify.nix
+++ home/programs/spicetify.nix
@@ -1,24 +1,26 @@
 {pkgs, spicetify-nix, ...}:
+let
+  spicePkgs = spicetify-nix.packages.${pkgs.system}.default;
+in
 {
   imports = [
     spicetify-nix.homeManagerModule
   ];
   programs.spicetify = {
     enable = true;
-    spotifyPackage = pkgs.spotify; # For some reason spictify only works with pkgs.spotify
-    theme = "catppuccin-mocha";
-    # OR
-    # theme = spicetify-nix.pkgSets.${pkgs.system}.themes.catppuccin-mocha;
+    spotifyPackage = pkgs.spotifywm; # For some reason spictify only works with pkgs.spotify
+    theme = spicePkgs.themes.catppuccin-mocha;
     colorScheme = "flamingo";
 
-    enabledExtensions = [
-      "fullAppDisplay.js"
-      "songStats.js"
-      "keyboardShortcut.js"
-      "fullAlbumDate.js"
-      "wikify.js"
-      "history.js"
-      "genre.js"
+    enabledExtensions =
+    with spicePkgs.extensions ; [
+      fullAppDisplay
+      songStats
+      keyboardShortcut
+      fullAlbumDate
+      wikify
+      history
+      genre
     ];
   };
 }
